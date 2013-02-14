3123e99735b549035285aed740d56073b64f934e
Erik + Vic <erik+vic@gmail.com>
2013-02-14 11:46:06 -0800

    Added styles and graphics

--- tictactest.js
+++ tictactest.js
@@ -1,83 +1,52 @@
 // Tic tac test
 
-// var tictactoe = require('./tictactoe.js')
+var runTurn = function(r,c, boxDiv){
+  try{
+    currentPlayer.makeMove(r,c);
+  }
+  catch(error){
+    console.log(error);
+    return;
+  }
+  switch(currentPlayer.name)
+  {
+    case "x":
+      $(boxDiv).css({"background-image":"url(./x.jpg)", "background-size":"100%"});
+      break;
+    case "o":
+      $(boxDiv).css({"background-image":"url(./o.jpg)", "background-size":"100%"});
+      break;
+  }
+  currentPlayer === ttt.p1 ? (currentPlayer = ttt.p2) : (currentPlayer = ttt.p1);
+  if(ttt.victory())
+  {
+    window.location = 'http://1.bp.blogspot.com/-ZR5Ttwex2ag/TphRF5lh0qI/AAAAAAAAADg/FsBwcOYDPgc/s1600/Victory%2521.jpg';
+  }
+  else if(ttt.staleMate())
+  {
+    $('.button').html("Replay");
+    $('.button').css({"height":"auto", "width":"100px", "padding":"10px", "margin":"auto",
+      "box-shadow":"10px 5px 5px #888888", "border":"2px solid grey"})
+  }
+  $('.player').html("Your turn, player " + currentPlayer.name);
+}
 
-// Set up Tic Tac Toe
+$('.board').click(function(e){
+  runTurn(e.target.id[1],e.target.id[2],'#'+e.target.id);
+});
+
+
+$('.button').click(function(e){
+  console.log("Clicked Restart")
+  document.location.reload(true)
+});
+
+
+// SCRIPTS
+
 ttt = makeTicTacToe();
 ttt.initialize();
 console.log(ttt.board);
-//ttt.printBoard();
-console.log(ttt.victory());
-
-//Play da game
 var currentPlayer = ttt.p2;
-while(!ttt.victory()) {
-  currentPlayer === ttt.p1 ? (currentPlayer = ttt.p2) : (currentPlayer = ttt.p1);
-  ttt.printBoard();
-  //alert(currentPlayer.name+" Make your move")
-  var r = prompt("Player " + currentPlayer.name + ", move to which row? (0,1,2)");
-  var c = prompt("Player " + currentPlayer.name + ", move to which col? (0,1,2)");
-  try{
-    currentPlayer.makeMove(r,c);
-    clear();
-  }
-  catch(error){
-    println(error);
-  }
-}
-window.location = 'http://1.bp.blogspot.com/-ZR5Ttwex2ag/TphRF5lh0qI/AAAAAAAAADg/FsBwcOYDPgc/s1600/Victory%2521.jpg';
+$('.player').html("Your turn, player " + currentPlayer.name);
 
-
-
-
-
-
-
-
-
-
-
-
-//TESTS
-
-
-// Take a turn
-// try{
-// ttt.p1.makeMove(1,1);
-// }
-// catch(error){
-//   println(error);
-// }
-// ttt.printBoard();
-
-// // Take a bad turn
-// try{
-// ttt.p1.makeMove(4,1);
-// }
-// catch(error){
-//   println(error);
-// }
-// ttt.printBoard();
-
-// Start running the ttt
-// while(!ttt.victory()){
-//   ttt.printTowers();
-//   var from = prompt("From tower (0,1,2)?");
-//   var to = prompt("WHERE TO???");
-//   fromTower = ttt.towers[from];
-//   toTower = ttt.towers[to];
-//   if (fromTower && toTower){
-//     try{
-//       ttt.makeMove(fromTower, toTower);
-//       clear();
-//     }
-//     catch(error){
-//       println(error);
-//     }
-//   }
-//   else{
-//     println("There is no spoon.");
-//   }
-
-// }
-// window.location = 'http://1.bp.blogspot.com/-ZR5Ttwex2ag/TphRF5lh0qI/AAAAAAAAADg/FsBwcOYDPgc/s1600/Victory%2521.jpg';
